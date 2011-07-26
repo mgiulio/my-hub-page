0ccfc3322b901015bbfc5b89bb0ae899751eff7e
mgiulio <giulio.mainardi@gmail.com>
2011-07-26 11:27:52 +0200

    Correct modal mask size computation

--- js/script.js
+++ js/script.js
@@ -78,8 +78,8 @@ var
 					position: 'fixed',
 					top: 0,
 					left: 0,
-					width: '1024px',
-					height: '768px',
+					width: '100%',
+					height: '100%',
 					opacity: 0.5,
 					'background-color': '#000000',
 					display: 'none'
