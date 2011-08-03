3f28c740a5a02551b2a2b82150451dbedf05ec4c
mgiulio <giulio.mainardi@gmail.com>
2011-08-03 09:46:46 +0200

    Refactoring js

--- js/script.js
+++ js/script.js
@@ -1,10 +1,5 @@
 $('body').addClass('js-enabled');
 
-$(document).keydown(function(e) {
-	if (e.which == 27)
-		closeBtn.click();
-});
-
 var 
 	modalMask = $('<div id="modal-mask"></div>').insertBefore('div.page:first'),
 	closeBtn = $('<a id="close-btn" href="#" title="Close. You can also press the Escape key">Close</a>')
@@ -23,6 +18,16 @@ $('#navbar').delegate('a', 'click',function() {
 	showPage($(this.href.replace(/.*#/, '#')));
 });
 
+keyboardShortcuts();	
+lifestream();
+
+if (window.location.hash)
+	showPage($(window.location.hash));
+
+/*
+ * Function definitions
+ */
+ 
 function showPage(p) {
 	p.append(closeBtn);
 	
@@ -30,32 +35,40 @@ function showPage(p) {
 		p.show(500);
 	});
 }
-			
-$(
-	'<div id="lifestream" class="page overlay">' + 
-		'<h2>' +
-			'<a href="http://christianv.github.com/jquery-lifestream/example.html">' + 
-				'<img src="img/jls-logo.png" title="Courtesy of jQuery Lifestream plugin">' +
-			'</a>' +
-			'Lifestream' +
-		'</h2>' +
-	'</div>')
-	.insertAfter('div.page:last')
-	.hide();
-$('<div class="content"></div>')
-	.appendTo('#lifestream')
-	.lifestream({
-		limit: 1000,
-		list: [
-			{ service: 'delicious', user: 'mgiulio' },
-			{ service: 'deviantart', user: 'giuliom' },
-			{ service: 'github', user: 'mgiulio' },
-			{ service: 'twitter', user: 'mgiulio' },
-			{ service: 'youtube', user: 'giuliomainardi0' }//,
-			//{ service: 'dribbble', user: 'mgiulio' }
-]});
-$('<li><a href="#lifestream" title="My activity on the Net">Lifestream</a></li>')
-	.insertBefore($($('#navbar').children().get(-1)));
 
-if (window.location.hash)
-	showPage($(window.location.hash));
+function lifestream() {
+	$(
+		'<div id="lifestream" class="page overlay">' + 
+			'<h2>' +
+				'<a href="http://christianv.github.com/jquery-lifestream/example.html">' + 
+					'<img src="img/jls-logo.png" title="Courtesy of jQuery Lifestream plugin">' +
+				'</a>' +
+				'Lifestream' +
+			'</h2>' +
+		'</div>')
+		.insertAfter('div.page:last')
+		.hide();
+	
+	$('<div class="content"></div>')
+		.appendTo('#lifestream')
+		.lifestream({
+			limit: 1000,
+			list: [
+				{ service: 'delicious', user: 'mgiulio' },
+				{ service: 'deviantart', user: 'giuliom' },
+				{ service: 'github', user: 'mgiulio' },
+				{ service: 'twitter', user: 'mgiulio' },
+				{ service: 'youtube', user: 'giuliomainardi0' }//,
+				//{ service: 'dribbble', user: 'mgiulio' }
+	]});
+
+	$('<li><a href="#lifestream" title="My activity on the Net">Lifestream</a></li>')
+		.insertBefore($($('#navbar').children().get(-1)));
+}
+
+function keyboardShortcuts() {
+	$(document).keydown(function(e) {
+		if (e.which == 27)
+			closeBtn.click();
+	});	
+}
