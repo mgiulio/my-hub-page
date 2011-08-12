c19ee7f123248e36a464d244760cf3a56640da43
mgiulio <giulio.mainardi@gmail.com>
2011-08-12 12:23:00 +0200

    Initial commit: remove code for slideshow

--- js/jquery.fullpgbgimg.js
+++ js/jquery.fullpgbgimg.js
@@ -1,16 +1,10 @@
 /* jQuery. */ var fullPgBgImg = function(userCfg) {
 	var
 		cfg = { // Set default configuration
-			//images: [],
-			duration: 10000, // How much time an image is displayed(not counting transition time)
-			transition: { // To control the smooth change between images
-				effect: 'cross fade',
-				duration: 3000 // ms - The time the transition takes to complete
-				//easing:
+			//image: '',
 			}
 		},
 		i, n, // Reusable vars
-		numImages,
 		techniques = [
 			{
 				name: 'CSS3 background-size',
@@ -35,32 +29,23 @@
 				},
 				apply: function() {
 					var
-						img = [],
-						visibleImg = 0
-						i = -1,
+						img,
 						w = $(window),
 						aspectRatio = 1024 / 768 // FIXME
 					;
 					
-					//Create the <img>'s
-					img.push(
-						$('<img src="' + cfg.images[++i] + '" alt="" style="position: fixed; top: 0; left: 0;">')
-							.prependTo('body')
-					);
-					if (numImages > 1) {
-						img.push(
-							$('<img src="' + cfg.images[++i] + '" alt="" style="position: fixed; top: 0; left: 0;">')
-							.prependTo('body')
-						);
-						visibleImg++;
-					}
+					//Create the <img>
+					img = $('<img src="' + cfg.image + '" alt="" style="position: fixed; top: 0; left: 0;">')
+						.prependTo('body')
+						.
+					;
 					
 					// Handle window resizing
 					w.resize(function() {
 						var 
 							wndWidth = w.width(),
 							wndHeight = w.height(),
-							imgNode = img[visibleImg].get(0)
+							imgNode = img.get(0)
 						;
 						if (aspectRatio < wndWidth / wndHeight) {
 							imgNode.width = wndWidth;
@@ -75,29 +60,12 @@
 							imgNode.style.top = '0px';
 						}
 					}).resize();
-					
-					if (numImages > 1)
-						window.setTimeout(function f() {
-							var nextImageUrl = cfg.images[i = (i+1) % numImages];
-								$.when(
-									img[visibleImg].fadeOut(cfg.transition.duration),
-									img[1-visibleImg].fadeIn(cfg.transition.duration)
-								).done(function() {
-									img[visibleImg].attr('src', nextImageUrl);
-									visibleImg = 1 - visibleImg;
-									aspectRatio = img[visibleImg].width() / img[visibleImg].height();
-									//w.resize();
-									window.setTimeout(f, cfg.duration);
-								});
-						}, cfg.duration);
 				}
 			}
 		]
 	;
 	
 	$.extend(cfg, userCfg);
-	
-	numImages = cfg.images.length;
 	
 	// Use the first feasible technique
 	// So, the techniques array must be sorted in ...
